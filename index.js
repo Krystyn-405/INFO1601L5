fc722641cee7f65128c541382c115c0f22ca04a2
Krystyn-405 <krystyn.dheurieux@gmail.com>
2025-03-05 10:46:53 -0400

    Created launch file, started editing (finished 4, onto 5).

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
 for(let i=0; i< arr.length; i++){
   console.log(arr[i])
 }
+
+let radius = 7;
+const pi = 3.14;
+
+let area = radius * radius * pi;
+
+let name = "bob";
+let age = 24;
+
+console.log (typeof(name));
+console.log (typeof(age));
+
+console.log (`Hello my name is ${name}, I am ${age} years old.`);
+
+console.log (`I was born in ${2020-age}`);
